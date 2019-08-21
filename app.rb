87dd567fa053dcdcd12ec6c7cbc8292a5c8b90e6
Audrea Cook <audreacwork@gmail.com>
2019-08-21 16:11:10 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Aud."
+  end
+
+  get '/hometown' do
+    "My hometown is Falcon, CO."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Snakes and Arrows.'"
+  end
 end
